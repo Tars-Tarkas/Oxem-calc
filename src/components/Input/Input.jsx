d644687cc6f43017be22a45415e6118095798d50
Молодык Андрей <tarstarkas00@mail.ru>
2022-11-07 18:34:55 +0700

    первая попытка реализовать маску ввода

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,47 +1,52 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import './Input.scss';
 
 
 
 const minmax ={
-    minValue: 0,
-    maxValue: 100000000
+    minValue: '0',
+    maxValue: '1000'
 }
 
 const Input= () =>{
-    const [price, setPrice] = useState(0);
+    const [price, setPrice] = useState('0');
 
-    const handleOnChangeRange = (e) => {
-        setPrice(e.target.value)      
+    const maskInput = useRef(0);
+
+    const handleChangeNumber = (e) =>{      
+        const numbers = e.target.value.replace(/(\D)/g, '');        
+        setPrice(numbers);        
+        console.log('price ' + numbers);
+    }
+    const handleChangeRange = (e) =>{
+        const numbers = e.target.value.replace(/\s/g, '');
+        setPrice(numbers);        
+        console.log('price ' + numbers);
     }
 
-    const handleOnChangeNumber = (e) => {
-        if (e.target.value > minmax.maxValue){
-            return false
-        }
-        setPrice(e.target.value)        
-    }
+    useEffect(() => {                
+        setPrice(maskInput.current.value.replace(/\B(?=(\d{3})+(?!\d))/g, " "))
+      }, [price]);
 
-
-    return (
+     return (
         <div className='input__block'>
         <input 
-            type='number' 
-            min={minmax.minValue} 
-            max={minmax.maxValue} 
-            onChange={(e) => handleOnChangeNumber(e)} 
+            type='text' 
+            maxLength={minmax.maxValue}                      
+            onChange={(e) => handleChangeNumber(e)} 
             value={price} 
-            className='input-number'
-            placeholder=""
+            ref={maskInput}
+            className='input-number'            
         />
+        {console.log(maskInput)}
 
         <input 
             type='range' 
-            min={minmax.minValue} 
-            max={minmax.maxValue} 
-            onChange={(e) => handleOnChangeRange(e)} 
-            value={price} 
+            min={minmax.minValue}
+            max={minmax.maxValue}
+            onChange={(e) => handleChangeRange(e)} 
+            value={price}
             className='input-range'
         />       
         </div>
